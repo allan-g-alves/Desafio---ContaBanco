6e2707eaf84bc1bf1039c47d3d7a41b3e5227195
Allan Alves <allan.g.alves8@gmail.com>
2024-09-19 12:27:08 -0300

    :pencil: Modifiquei o texto da mensagem retornada para incluir R$

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -28,7 +28,7 @@ public class ContaTerminal {
         System.out.println("Digite o saldo:");
         double saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nomeCliente + "! " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + numero + " e seu saldo " + saldo + " já está disponível para saque!");
+        System.out.println("Olá " + nomeCliente + "! " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + numero + " e seu saldo é de R$" + saldo + " já está disponível para saque!");
         
         scanner.close();
     }
